97ebc49cc2b42d5bec4db588c906b44eb1b04cd5
voy13k <voy13k@github.com>
2014-08-13 16:24:15 +1000

    Added units after EditText fields on FillUpFragment.

---
+++ src/me/voy13k/lpglog/ToDo.java
@@ -0,0 +1,10 @@
+package me.voy13k.lpglog;
+
+public class ToDo {
+
+    private int applyCommonStylesToFillUp;
+    private int fixStylingOnLog;
+    private int addYearSectionsOnLog;
+    private int fixHorizontalInputLabelsOnFillUp;
+    private int rethingDaoDataArrangement;
+}
